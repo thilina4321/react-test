891571430a348ca2aee54fec1e99eacc9c992f73
thilina_dilshan <thilina.dilshan@arimaclanka.com>
2022-10-25 20:20:12 +0530

    state change test with button click

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <div>Hello world</div>
       {isLogin ? <div>Welcome</div> : <div>Please login first</div> }
+      <button onClick={()=>setIsLogin(true)}> Login </button>
     </div>
   );
 }
