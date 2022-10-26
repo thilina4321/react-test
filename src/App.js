4ce3cf8d44eb5791cba27b83cf347bf520ac5db8
thilina_dilshan <thilina.dilshan@arimaclanka.com>
2022-10-26 16:41:24 +0530

    async mock test case

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   const [isLogin, setIsLogin] = useState(false)
+  const [properties, setProperties] = useState([])
+
+  useEffect(() => {
+     fetchProperties()
+  }, [])
+
+  const fetchProperties = async()=>{
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+    const jsonResponse = await response.json()
+    setProperties(()=> [...jsonResponse])
+  }
+
+  
   return (
     <div className="App">
       <div>Hello world</div>
       {isLogin ? <div>Welcome</div> : <div>Please login first</div> }
       <button onClick={()=>setIsLogin(true)}> Login </button>
+
+      <ul>
+      { properties.map(property=> <li key={property.userId}> {property.userId} </li> ) }
+      </ul>
     </div>
   );
 }
